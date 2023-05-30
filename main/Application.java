91b1842a7be4aeda7e6365e54c17c43b1635d278
VictorCorlateanu <126675107+VictorCorlateanu@users.noreply.github.com>
2023-05-30 11:15:59 +0300

    Add files via upload
    This version has added a new package "world" with a new class "Liquid", that has properties and methods for objects of proper liquid types.

--- main/Application.java
+++ main/Application.java
@@ -3,27 +3,37 @@ package main;
 import kitchen.Cup;
 import kitchen.SmallCup;
 import kitchen.BigCup;
+import world.Liquid;
 
 public class Application {
     public static void main(String[] args) {
         
-        Cup cupA = new Cup("Milk", (short)300);
-        Cup cupB = new Cup("Water", (short)500);
+        //Cup cupA = new Cup("Milk", (short)300);
+        //Cup cupB = new Cup("Water", (short)500);
 
-        cupA.setLiquidVolume((short) (cupA.getLiquidVolume() - (short)50));
-        cupB.setLiquidVolume((short) (cupB.getLiquidVolume() + (short)50));
+        //cupA.setLiquidVolume((short) (cupA.getLiquidVolume() - (short)50));
+        //cupB.setLiquidVolume((short) (cupB.getLiquidVolume() + (short)50));
 
-        Cup smallCupA = new SmallCup("Tea", (short)200);
-        System.out.println("smallcupA contains: \n" + smallCupA);
+        //Cup smallCupA = new SmallCup("Tea", (short)200);
+        //System.out.println("smallcupA contains: \n" + smallCupA);
 
 
-        Cup bigCupA = new BigCup("Water", (short)200);
+        //Cup bigCupA = new BigCup("Water", (short)200);
 
-        bigCupA.setLiquidVolume((short) (bigCupA.getLiquidVolume() + cupB.getLiquidVolume()));
-        cupB.setLiquidVolume   ((short) (cupB.getLiquidVolume() - cupB.getLiquidVolume()));
+        //bigCupA.setLiquidVolume((short) (bigCupA.getLiquidVolume() + cupB.getLiquidVolume()));
+        //cupB.setLiquidVolume   ((short) (cupB.getLiquidVolume() - cupB.getLiquidVolume()));
 
-        System.out.println("bigCupA contains: \n" + bigCupA);
-        System.out.println("cupB contains: \n" + cupB);
+        //System.out.println("bigCupA contains: \n" + bigCupA);
+        //System.out.println("cupB contains: \n" + cupB);
+
+        Liquid liqA = new Liquid("Water", 150, (byte)18);
+        Liquid liqB = new Liquid("Milk", 200, (byte)30);
+        Liquid liqC = new Liquid("Tea", 300, (byte)46);
+
+        System.out.println(liqA);
+        System.out.println(liqB);
+        System.out.println(liqC);
+
 
     }
 }
---
+++ world/Liquid.java
@@ -0,0 +1,81 @@
+package world;
+
+public class Liquid {
+    
+    private String name;
+    private int    volume;           // ml
+    private byte   temperature;      // Celsius
+
+    public Liquid () {
+
+    }
+
+    public Liquid (String name, int volume, byte temperature) {      //Polymorphism
+        setName(name);
+        setVolume(volume);
+        setTemperature(temperature);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if ( !name.isBlank() && !name.equals("null") ) {
+            this.name = name;
+        } else {
+            System.out.println("Cannot set a whitespace or the \"null\"-string as a name. Try again!");
+        }
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        if ( volume > 0 ) {
+            this.volume = volume;
+        } else {
+            System.out.println("Cannot set a negative or 0 ml. Try again!");
+        }
+    }
+
+    public byte getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(byte temperature) {
+        if ( temperature >= 0 && temperature <= 100 )
+        this.temperature = temperature;
+    }
+
+    public boolean isCold() {
+        boolean a = false;
+        if  (getTemperature() <= (byte)18 ) {
+        a = true;
+        }
+        return a;
+    }
+
+    public boolean isWarm() {
+        boolean a = false;
+        if ( getTemperature() > (byte)18 && getTemperature() <= (byte)36 ) {
+        a = true;
+        }
+        return a;
+    }
+
+    public boolean isHot() {
+        boolean a = false;
+        if ( getTemperature() > (byte)36 ) {
+        a = true;
+        }
+        return a;
+    }
+
+    public String toString() {
+        String out = "Name: " + name + "\nVolume: " + volume + " ml\nTemperature: " + temperature + " Celsius\n";
+        return out;
+    }
+
+}
